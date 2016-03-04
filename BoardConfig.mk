00c780fa6797813a9894127a3dda0555b71c97af
Rohan Taneja <rohantaneja63@gmail.com>
2016-03-04 17:09:17 +0530

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,134 @@
+# inherit from the proprietary version
+-include vendor/mediatek/mt6582/BoardConfigVendor.mk
+
+# GPS
+TARGET_SPECIFIC_HEADER_PATH := device/mediatek/mt6582/include
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6582
+TARGET_NO_BOOTLOADER := true
+
+# Architecture
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+TARGET_CPU_VARIANT := cortex-a7
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := mt6582
+
+# MTK Hardware
+BOARD_HAS_MTK_HARDWARE := true
+MTK_HARDWARE := true
+COMMON_GLOBAL_CFLAGS += -DMTK_HARDWARE -DADD_LEGACY_ACQUIRE_BUFFER_SYMBOL
+COMMON_GLOBAL_CPPFLAGS += -DMTK_HARDWARE
+
+# Partitions
+# make_ext4fs requires numbers in dec format
+BOARD_BOOTIMAGE_PARTITION_SIZE := 6291456
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 8629568
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 891289600
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_USERIMAGES_USE_EXT4:=true
+TARGET_USERIMAGES_SPARSE_EXT_DISABLED := false
+
+# Kernel
+BOARD_KERNEL_CMDLINE :=
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+
+# Flags
+TARGET_GLOBAL_CFLAGS   += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+
+TARGET_KMODULES := true
+
+COMMON_GLOBAL_CFLAGS += -DDISABLE_HW_ID_MATCH_CHECK
+TARGET_RUNNING_WITHOUT_SYNC_FRAMEWORK := true
+
+TARGET_PREBUILT_KERNEL := device/mediatek/mt6582/kernel
+BOARD_CUSTOM_BOOTIMG_MK := device/mediatek/mt6582/bootimg.mk
+BOARD_MKBOOTIMG_ARGS := --board 1457085595
+BOARD_CUSTOM_BOOTIMG := true
+#TARGET_PREBUILT_RECOVERY_KERNEL :=
+
+TARGET_RECOVERY_FSTAB := device/mediatek/mt6582/rootdir/root/twrp.fstab
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := mt6582,aurora
+
+# TWRP
+DEVICE_RESOLUTION := 720x1280
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+RECOVERY_GRAPHICS_USE_LINELENGTH := true
+TW_NO_REBOOT_BOOTLOADER := true
+TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/musb-hdrc.0/gadget/lun%d/file
+TW_MAX_BRIGHTNESS := 255
+TW_INTERNAL_STORAGE_PATH := "/emmc"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "emmc"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p7"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
+TW_EXCLUDE_SUPERSU := true
+TW_INCLUDE_FB2PNG := true
+
+# Deodex
+WITH_DEXPREOPT := false
+DISABLE_DEXPREOPT := true
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_MTK := true
+BOARD_BLUETOOTH_DOES_NOT_USE_RFKILL := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/mediatek/mt6582/bluetooth
+
+# EGL settings
+BOARD_EGL_CFG := device/mediatek/mt6582/rootdir/configs/egl.cfg
+USE_OPENGL_RENDERER := true
+BOARD_EGL_WORKAROUND_BUG_10194508 := true
+
+# Disable memcpy opt (for audio libraries)
+TARGET_CPU_MEMCPY_OPT_DISABLE := true
+
+# SELINUX
+BOARD_SEPOLICY_DIRS := \
+       device/mediatek/mt6582/sepolicy
+
+BOARD_SEPOLICY_UNION := \
+       device.te \
+       app.te \
+       system.te \
+       netd.te \
+       file_contexts
+
+TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/virtual/android_usb/android0/f_mass_storage/lun%d/file"
+
+# Offline charging
+ADDITIONAL_DEFAULT_PROPERTIES += ro.mount.fs=EXT4
+
+# RIL
+BOARD_RIL_CLASS := ../../../device/mediatek/mt6582/ril/
+
+BOARD_CONNECTIVITY_VENDOR := MediaTek
+BOARD_CONNECTIVITY_MODULE := conn_soc
+
+# WIFI
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_mt66xx
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_mt66xx
+WIFI_DRIVER_FW_PATH_PARAM:="/dev/wmtWifi"
+WIFI_DRIVER_FW_PATH_STA:=STA
+WIFI_DRIVER_FW_PATH_AP:=AP
+WIFI_DRIVER_FW_PATH_P2P:=P2P
+
---
+++ README.md
@@ -0,0 +1,53 @@
+Generic MediaTek MT6582 device.
+==============
+
+Basic   | Spec Sheet
+-------:|:-------------------------
+CPU     | 1.3GHz Quad-Core MT6582
+GPU     | Mali-400MP
+Memory  | 1GB RAM
+Shipped Android Version | 4.2.2 - 4.4.2
+Storage | 8GB (varies)
+Display | 6.0" 1280 x 720 px (varies)
+Camera  | 8MPx, LED Flash
+
+This branch is for building CyanogenMod 12.1 (or CM-12.1 based) ROMS.
+
+# Build
+
+* Working
+  * Dual SIM
+  * Wifi
+  * Bluetooth
+  * Audio
+  * Sensors
+  * Camera (photo and video recording)
+  * GPS
+  * NFC (removed)
+  * OTG
+  * Tethering (Wifi, Bluetooth and USB)
+  * FM Radio (removed)
+
+* Compilation
+
+        # repo init -u git://github.com/rohantaneja/android.git -b cm-12.1
+        
+        # repo sync
+        
+        # source build/envsetup.sh
+        
+        # brunch cm_mt6582-userdebug
+
+# MTK
+
+Few words about mtk related binaries, services and migration peculiarities.
+
+# Limitations
+
+Services requires root:
+
+`system/core/rootdir/init.rc`
+
+  * surfaceflinger depends on sched_setscheduler calls, unable to change process priority from 'system' user (default user 'system')
+
+  * mediaserver depends on /data/nvram folder access, unable to do voice calls from 'media' user (default user 'media')
