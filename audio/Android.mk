916f8fff4dd3702e0d51290897b492774c238d07
Rohan Taneja <rohantaneja63@gmail.com>
2016-04-26 16:26:46 +0530

    audio: Android.mk mt6592 -> mt6582

--- audio/Android.mk
+++ audio/Android.mk
@@ -4,16 +4,16 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES:= audio.cpp
 
-LOCAL_MODULE := audio.primary.mt6592
+LOCAL_MODULE := audio.primary.mt6582
 LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
 LOCAL_C_INCLUDES += frameworks/av/media/mtp/ system/core/include/ frameworks/rs/server/ frameworks/av/include/ hardware/libhardware/include/
-#LOCAL_LDFLAGS += -L$(LOCAL_PATH)/../../../../vendor/mediatek/mt6592/lib/ -laudio.primary.default
+#LOCAL_LDFLAGS += -L$(LOCAL_PATH)/../../../../vendor/mediatek/mt6582/lib/ -laudio.primary.default
 LOCAL_SHARED_LIBRARIES := libdl libcutils liblog libutils libbinder
 
 include $(BUILD_SHARED_LIBRARY)
 
 LIBORIG := $(LOCAL_INSTALLED_MODULE)
-LIBLINK1 := $(subst audio.primary.mt6592,audio.primary.default,$(LIBORIG))
+LIBLINK1 := $(subst audio.primary.mt6582,audio.primary.default,$(LIBORIG))
 $(LIBLINK1): $(LIBORIG)
 	@echo "Symlink: $@ -> $(notdir $<)"
 	@mkdir -p $(dir $@)
